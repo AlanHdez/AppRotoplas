63a255b9061f0e4c82c1f3a5563b93363d963fd4
alanhdezl <33246453+alanhdezl@users.noreply.github.com>
2018-07-22 03:48:42 -0500

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,27 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
+import { RegistroPage } from '../pages/registro/registro';
+import { MsmPage } from '../pages/msm/msm';
+import { PanelPage } from '../pages/panel/panel';
+import { PerfilPage } from '../pages/perfil/perfil';
+import { RotoplasServicios } from '../servicios/rotoplas.servicios';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB_dGUVmTWg-gN_GBRORHz89vFGbhFzgwE",
+  authDomain: "rotoplasapp-12198.firebaseapp.com",
+  databaseURL: "https://rotoplasapp-12198.firebaseio.com",
+  projectId: "rotoplasapp-12198",
+  storageBucket: "rotoplasapp-12198.appspot.com",
+  messagingSenderId: "594354482315"};
 
 @NgModule({
   declarations: [
@@ -17,11 +35,19 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    LoginPage,
+    MsmPage,
+    PanelPage,
+    PerfilPage,
+    RegistroPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,12 +55,18 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    LoginPage,
+    MsmPage,
+    PanelPage,
+    PerfilPage,
+    RegistroPage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    RotoplasServicios
   ]
 })
 export class AppModule {}
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
+import { NavParams } from 'ionic-angular';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { PerfilPage } from '../perfil/perfil';
+import { PanelPage } from '../panel/panel';
+import { MsmPage } from '../msm/msm';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
+  tab1Root = PerfilPage;
+  tab2Root = PanelPage;
+  tab3Root = MsmPage;
+  infousuario:any;
+    constructor(public navParams: NavParams) {
+      this.infousuario = {
+        id:this.navParams.get('id'),
+        tipo:this.navParams.get('tipo'),
+        email:this.navParams.get('email'),
+        nombre:this.navParams.get('nombre'),
+        idDisp:this.navParams.get('idDisp'), 
+        user:this.navParams.get('user'),
+        password:this.navParams.get('password')
+      } 
+    }
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
-
-  constructor() {
-
-  }
 }
